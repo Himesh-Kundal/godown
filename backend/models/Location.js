323017ca91b56cd153646b72b062398572d7f8bf
Himesh-Kundal <himesh.kundal@kgpian.iitkgp.ac.in>
2024-10-11 14:53:26 +0530

    uncomment

--- backend/models/Location.js
+++ backend/models/Location.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   parent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', default: null }
+  name: { type: String, required: true },
+  parent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', default: null }
 });
 
 module.exports = mongoose.model('Location', locationSchema);
